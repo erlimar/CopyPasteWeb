48cbea2fb434adb3c65c3c5ed7cb179f72f7fb8e
Erlimar Silva Campos <erlimar@gmail.com>
2017-10-06 20:23:28 -0300

    Versao inicial

---
+++ CopyPasteWeb/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CopyPastWeb
+{
+    public static class Constants
+    {
+        public static string ProductName = "Copy & Paste";
+        public static string ProductDescription = "Copiar e Colar na Web entre máquinas";
+    }
+}
---
+++ CopyPasteWeb/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CopyPastWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.UrlNewSession = Url.Action("NewSession");
+            ViewBag.UrlSession = Url.Action("Session");
+
+            return View();
+        }
+
+        public ActionResult NewSession()
+        {
+            return Content(Guid.NewGuid().ToString("N"));
+        }
+
+        public ActionResult Session(string id)
+        {
+            ViewBag.UrlHome = Url.Action("Index");
+            ViewBag.SessionID = id;
+
+            return View();
+        }
+
+        public ActionResult Send(string content)
+        {
+
+        }
+
+        public ActionResult Receive()
+        {
+
+        }
+    }
+}
