1308c16334baa05e90a9b4d417372ef3124ec033
Erlimar Silva Campos <erlimar.campos@squadra.com.br>
2017-10-09 14:10:44 -0300

    Implementa parcialmente primeira versão

--- CopyPasteWeb/Controllers/HomeController.cs
+++ CopyPasteWeb/Controllers/HomeController.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.SessionState;
 
 namespace CopyPastWeb.Controllers
 {
     public class HomeController : Controller
     {
+        public const string SESSION_ACTION_NAME = "Session";
+        private readonly Database _db;
+
+        public HomeController()
+        {
+            _db = Database.Instance;
+        }
+
         public ActionResult Index()
         {
-            ViewBag.UrlNewSession = Url.Action("NewSession");
-            ViewBag.UrlSession = Url.Action("Session");
+            ViewBag.UrlNewSession = Url.Action(nameof(NewSession));
+            ViewBag.UrlSession = Url.Action(SESSION_ACTION_NAME);
 
             return View();
         }
@@ -21,22 +31,25 @@ namespace CopyPastWeb.Controllers
             return Content(Guid.NewGuid().ToString("N"));
         }
 
-        public ActionResult Session(string id)
+        [ActionName(SESSION_ACTION_NAME)]
+        public ActionResult SessionAction(string id)
         {
-            ViewBag.UrlHome = Url.Action("Index");
+            ViewBag.UrlHome = Url.Action(nameof(Index));
+            ViewBag.UrlSend = Url.Action(nameof(Send));
+            ViewBag.UrlReceive = Url.Action(nameof(Receive));
             ViewBag.SessionID = id;
 
             return View();
         }
 
-        public ActionResult Send(string content)
+        public ActionResult Send(string content, string sessionID)
         {
-
+            return Content(_db.SetValue(sessionID, content));
         }
 
-        public ActionResult Receive()
+        public ActionResult Receive(string sessionID)
         {
-
+            return Content(_db.GetValue(sessionID));
         }
     }
 }
---
+++ CopyPasteWeb/Database.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CopyPastWeb
+{
+    public class Database
+    {
+        private static Database _instance;
+        private readonly IDictionary<string, string> _database;
+
+        private Database()
+        {
+            _database = new Dictionary<string, string>();
+        }
+
+        public static Database Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Database();
+                }
+
+                return _instance;
+            }
+        }
+
+        public string GetValue(string sessionID)
+        {
+            if (!_database.ContainsKey(sessionID))
+            {
+                return string.Empty;
+            }
+
+            return _database[sessionID];
+        }
+
+        public string SetValue(string sessionId, string newValue)
+        {
+            _database[sessionId] = newValue;
+
+            return GetValue(sessionId);
+        }
+    }
+}
